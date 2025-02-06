a2885ea01d4f7351728fb0330c771803ecf1dedc
kani <kanishkas@usf.edu>
2025-02-06 15:18:29 -0500

    Task 1 - Customer Profile

---
+++ cc_5.js
@@ -0,0 +1,13 @@
+// Task 1: Customer Profile
+
+// Creating the customer object with properties
+const customer = {
+    name: "John Doe",
+    age: 35,
+    email: "john.doe@email.com"
+};
+
+// Logging each property using template literals
+console.log(`Name: ${customer.name}`);
+console.log(`Age: ${customer.age}`);
+console.log(`Email: ${customer.email}`);
